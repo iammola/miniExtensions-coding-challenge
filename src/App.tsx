90b050434530f8b431970feb6911d9178a643afb
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-09 12:39:53 +0100

    🚧wip: add login function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,40 @@
+import Airtable from "airtable";
+import { useDispatch } from "react-redux";
+
+import { setUser, setIsLoggedIn } from "features";
+
+const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_API_KEY }).base("app8ZbcPx7dkpOnP0");
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+
+  async function login(user: string) {
+    dispatch(setUser(user));
+    dispatch(setIsLoggedIn(false));
+
+    const [student] = await base("Students")
+      .select({ filterByFormula: `({Name}="${user}")` })
+      .all();
+
+    if (student == undefined) return;
+
+    const classes = await base("Classes")
+      .select({ filterByFormula: `OR(${(student.get("Classes") as string[])?.map((id) => `RECORD_ID()="${id}"`)})` })
+      .all();
+
+    const mates = await base("Students")
+      .select({ filterByFormula: `OR(${classes.map((cl) => `SEARCH("${cl.get("Name")}", {Classes})`)})` })
+      .all();
+
+    const data = classes.map((item) => ({
+      name: item.get("Name") as string,
+      students: (item.get("Students") as string[]).map((student) =>
+        mates.find((mate) => mate.id === student)?.get("Name")
+      ) as string[],
+    }));
+
+    dispatch(setIsLoggedIn(true));
+  }
+
   return <></>;
 };
 
--- src/features/Form/index.tsx
+++ src/features/Form/index.tsx
@@ -2,12 +2,13 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { setValue } from "./slice";
 
-export const Form: React.FC = () => {
+export const Form: React.FC<{ login(user: string): void }> = ({ login }) => {
   const dispatch = useDispatch();
   const value = useSelector((state: Types.State) => state.form.value);
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    login(value);
   }
 
   return (
